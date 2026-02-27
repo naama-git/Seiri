25ea4b9ec37967b419a93970b7d0ee96ef098957
naama_code <naanas344@gmail.com>
2026-02-27 12:33:41 +0200

    Defined ESlint file

--- seiri-backend/README.md
+++ seiri-backend/README.md
@@ -1,18 +1,19 @@
 # Seiri Backend
 
 ## Todo list
-- [x] Open DB
+- [x] Open DB connection
 - [ ] Build entities 
     1. [ ] User
-    2. [ ] General
-    3. [ ]File 
+    2. [ ] FileSystemItem
+    3. [ ] FileMetadata
 - [ ] Build services
     * user CRUD
-    * file CRUD
+    * file CRUD (Mock Functions)
+    * file actions
 - [ ] Build controllers
 - [ ] Auth
 - [ ] Seeding
 - [ ] Logging
 - [ ] Validation
 - [ ] Exceptions
-- [ ] Caching
+- [ ] Caching?
--- seiri-backend/eslint.config.mjs
+++ seiri-backend/eslint.config.mjs
@@ -1,37 +1,60 @@
 // @ts-check
+// eslint.config.mjs
 import eslint from '@eslint/js';
-import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
-import globals from 'globals';
 import tseslint from 'typescript-eslint';
+import globals from 'globals';
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
 
 export default tseslint.config(
-  {
-    ignores: ['eslint.config.mjs'],
-  },
-  eslint.configs.recommended,
-  ...tseslint.configs.recommendedTypeChecked,
-  eslintPluginPrettierRecommended,
-  {
-    languageOptions: {
-      globals: {
-        ...globals.node,
-        ...globals.jest,
-      },
-      sourceType: 'commonjs',
-      parserOptions: {
-        projectService: true,
-        tsconfigRootDir: import.meta.dirname,
+ 
+  [
+    {
+      ignores: ['eslint.config.mjs'],
+    },
+    eslint.configs.recommended,
+    ...tseslint.configs.recommendedTypeChecked,
+    eslintPluginPrettierRecommended,
+    {
+      languageOptions: {
+        globals: {
+          ...globals.node,
+          ...globals.jest,
+        },
+        parserOptions: {
+          projectService: true,
+          tsconfigRootDir: import.meta.dirname,
+        },
       },
     },
-  },
-  {
-    rules: {
-      '@typescript-eslint/no-explicit-any': 'off',
-      '@typescript-eslint/no-floating-promises': 'warn',
-      '@typescript-eslint/no-unsafe-argument': 'warn',
-      "prettier/prettier": ["error", { endOfLine: "auto" }],
-      
-      "quotes": ["error", "single"]
+    {
+      rules: {
+        '@typescript-eslint/no-explicit-any': 'off',
+        '@typescript-eslint/no-floating-promises': 'warn',
+        '@typescript-eslint/no-unsafe-argument': 'warn',
+        'prettier/prettier': ['error', { endOfLine: 'auto' }],
+        'quotes': ['error', 'single'],
+
+
+        '@typescript-eslint/naming-convention': [
+        'error',
+          {
+            selector: 'default',
+            format: ['camelCase'],
+            leadingUnderscore: 'allow', 
+          },
+          // Interfaces, Classes,Enums...
+          {
+            selector: 'typeLike',
+            format: ['PascalCase'],
+          },
+         
+          {
+            selector: 'variable',
+            modifiers: ['const'],
+            format: ['camelCase', 'UPPER_CASE'],
+          },
+        ],
+      },
     },
-  },
+  ] 
 );
