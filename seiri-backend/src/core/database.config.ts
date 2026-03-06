fd6b915c7dbb556e34d95eed9c75a6b03ff99359
naama_code <naanas344@gmail.com>
2026-03-06 13:26:01 +0200

    Define data-source file, migrations scripts and database configuration

---
+++ seiri-backend/src/core/database.config.ts
@@ -0,0 +1,17 @@
+import { registerAs } from '@nestjs/config';
+import { DataSourceOptions } from 'typeorm';
+
+export default registerAs(
+  'database',
+  (): DataSourceOptions => ({
+    type: 'postgres',
+    host: process.env.DB_HOST,
+    port: parseInt(process.env.DB_PORT || '5432', 10),
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+    migrations: [__dirname + '/../migrations/*{.ts,.js}'],
+    synchronize: false,
+  }),
+);
