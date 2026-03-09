e8093d14c4236c69c04952afc45929c18ffe0460
naama_code <naanas344@gmail.com>
2026-03-09 21:38:18 +0200

    add swagger bearer

--- seiri-backend/src/core/swagger.config.ts
+++ seiri-backend/src/core/swagger.config.ts
@@ -7,6 +7,17 @@ export function setupSwagger(app: INestApplication, apiPrefix: string): void {
     .setDescription('The Seiri API documentation')
     .addTag('Main')
     .addServer(`/${apiPrefix}`)
+    .addBearerAuth(
+      {
+        type: 'http',
+        scheme: 'bearer',
+        bearerFormat: 'JWT',
+        name: 'JWT',
+        description: 'Enter JWT token',
+        in: 'header',
+      },
+      'access-token',
+    )
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
