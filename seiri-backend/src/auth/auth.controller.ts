facd2f42b48f2d23d91d539f6efbc04ef41b5af9
naama_code <naanas344@gmail.com>
2026-03-23 17:48:39 +0200

    small fix in auth controller

--- seiri-backend/src/auth/auth.controller.ts
+++ seiri-backend/src/auth/auth.controller.ts
@@ -1,26 +1,30 @@
 import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport/dist/auth.guard';
 import { AuthService } from './auth.service';
-import { CreateUserDto, LoginUserDTO } from 'src/user/User.dto';
+import { CreateUserDto, LoginUserDTO, ReadUserDto } from 'src/user/User.dto';
 import type { AuthenticatedUser, LoginResponse } from './auth.interface';
 import { currentUser } from './current-user.decorator';
 import { ApiBearerAuth } from '@nestjs/swagger/dist/decorators/api-bearer.decorator';
 import { User } from 'src/user/user.entity';
+import { serialize } from 'src/user/user.interceptor';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
+  @serialize(ReadUserDto)
   @Post('login')
   async login(@Body() loginDto: LoginUserDTO): Promise<LoginResponse> {
     return await this.authService.logIn(loginDto);
   }
 
+  @serialize(ReadUserDto)
   @Post('register')
   async register(@Body() createUserDto: CreateUserDto): Promise<User> {
     return await this.authService.register(createUserDto);
   }
 
+  @serialize(ReadUserDto)
   @ApiBearerAuth('access-token')
   @UseGuards(AuthGuard('jwt'))
   @Get('profile')
